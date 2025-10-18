b40f5dc1a875f5ea46877b5303464d4133a5d72e
Xavier Ibarra <xibarraprieto2003@gmail.com>
2025-10-18 15:37:07 -0500

    Listo Api

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/api/users', userRoutes);
 app.use('/api', catalogRoutes);
 
 app.get('/', (req, res) => {
-  res.send('API de ProStore funcionando!');
+  res.send('API funcionando!');
 });
 
 
